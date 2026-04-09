b5adb95afb463996fa85ad0787945b02651f6227
NeoEmo <155008342+NeoEmo@users.noreply.github.com>
2026-04-09 11:25:23 +0300

    Исправление версий

--- src/main/java/org/fxstudy/encoderdecryptofx/HelloApplication.java
+++ src/main/java/org/fxstudy/encoderdecryptofx/HelloApplication.java
@@ -14,7 +14,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("EncoderDecryptMain.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 640, 800);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/org/fxstudy/encoderdecryptofx/Theme.css")).toExternalForm());
-        stage.setTitle("EncoderDecryptoFX");
+        stage.setTitle("EncoderDecryptoFX 0.1");
         stage.setScene(scene);
         stage.show();
     }
